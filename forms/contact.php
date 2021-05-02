dbc53ca9dc2609397d4304a6628c08849d017521
ushan sankalpa <ushan.20191142@iit.ac.lk>
2021-05-02 17:30:48 +0530

    mail to

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'ushansankalpa95@gmail.com';
+  /**$receiving_email_address = 'ushansankalpa95@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -24,18 +24,44 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+ 
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
+    'host' => 'localhost',
+    'username' => 'root',
+    'password' => '',
+    'port' => '3306'
   );
-  */
+  
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
 
-  echo $contact->send();
+  echo $contact->send();**/
+  
+  if(isset($_POST['btn-send']))
+    {
+       $UserName = $_POST['name'];
+       $Email = $_POST['email'];
+       $Subject = $_POST['subject'];
+       $Msg = $_POST['message'];
+
+       if(empty($UserName) || empty($Email) || empty($Subject) || empty($Msg))
+       {
+           header('location:index.php?error');
+       }
+       else
+       {
+           $to = "ushansankalpa95@gmail.com";
+
+           if(mail($to,$Subject,$Msg,$Email))
+           {
+               header("location:index.html?success");
+           }
+       }
+    }
+    else
+    {
+        header("location:index.html");
+    }
 ?>
